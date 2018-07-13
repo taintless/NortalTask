30adcaf06b3400d6c179fe7abb74fb6c75c6dc43
JokubasRaila <jokubas.raila@gmail.com>
2018-07-13 22:12:21 +0300

    Fix IProductsService spelling mistake

--- eShop/eShop/DataContracts/IProductsRepository.cs
+++ eShop/eShop/DataContracts/IProductsRepository.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace eShop.DataContracts
 {
-    public interface IProducsRepository
+    public interface IProductsRepository
     {
         Task<List<int>> GetDifferentStorages();
     }
