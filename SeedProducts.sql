6a94855c12fb0242cf328ba717f7bce9c2dbbb12
JokubasRaila <jokubas.raila@gmail.com>
2018-07-15 12:00:45 +0300

    Seed Products fix manufacturers

--- SeedProducts.sql
+++ SeedProducts.sql
@@ -8,26 +8,26 @@ INSERT INTO Products ([OsId]
 , [Price]
 , [Storage])
   VALUES 
-  (3, 1, 15, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
+  (1, 1, 15, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
     Maecenas luctus ante in neque maximus, sed viverra sem posuere. Vestibulum lectus nisi, laoreet vel suscipit nec, feugiat at odio. Etiam eget tellus arcu.', 'sony-xperia-z3-large.jpg', 'sony-xperia-z3.jpg', 'Sony Xperia Z3', 899, 16),
 
-  (3, 3, 5, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
+  (1, 3, 5, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
     Maecenas luctus ante in neque maximus, sed viverra sem posuere. Vestibulum lectus nisi, laoreet vel suscipit nec, feugiat at odio. Etiam eget tellus arcu.', 'htc-one-large.jpg', 'htc-one.jpg', 'HTC One M8', 899, 32), 
 
-  (4, 2, 8, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
+  (2, 2, 8, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
     Maecenas luctus ante in neque maximus, sed viverra sem posuere. Vestibulum lectus nisi, laoreet vel suscipit nec, feugiat at odio. Etiam eget tellus arcu.', 'iphone6-large.jpg', 'iphone6.jpg', 'Iphone 6', 899, 16),
 
-  (3, 4, 12, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
+  (1, 4, 12, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
     Maecenas luctus ante in neque maximus, sed viverra sem posuere. Vestibulum lectus nisi, laoreet vel suscipit nec, feugiat at odio. Etiam eget tellus arcu.', 'galaxy-alpha-large.jpg', 'galaxy-alpha.jpg', 'Galaxy Alpha', 899, 32),
 
-  (5, 5, 5, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
+  (3, 5, 5, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
     Maecenas luctus ante in neque maximus, sed viverra sem posuere. Vestibulum lectus nisi, laoreet vel suscipit nec, feugiat at odio. Etiam eget tellus arcu.', 'nokia-lumia-large.jpg', 'nokia-lumia.jpg', 'Nokia Lumia', 899, 16),
 
-  (3, 6, 12, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
+  (1, 6, 12, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
     Maecenas luctus ante in neque maximus, sed viverra sem posuere. Vestibulum lectus nisi, laoreet vel suscipit nec, feugiat at odio. Etiam eget tellus arcu.', 'zte-nubia-large.jpg', 'zte-nubia.jpg', 'Zte Nubia', 899, 32),
 
-  (3, 4, 15, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
+  (1, 4, 15, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
     Maecenas luctus ante in neque maximus, sed viverra sem posuere. Vestibulum lectus nisi, laoreet vel suscipit nec, feugiat at odio. Etiam eget tellus arcu.', 'galaxy-s5-large.jpg', 'galaxy-s5.jpg', 'Samsung Galaxy S5', 899, 16),
 
-  (4, 2, 8, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
+  (2, 2, 8, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra.
     Maecenas luctus ante in neque maximus, sed viverra sem posuere. Vestibulum lectus nisi, laoreet vel suscipit nec, feugiat at odio. Etiam eget tellus arcu.', 'iphone5s-large.jpg', 'iphone5s.jpg', 'Iphone 5S', 899, 16)
